bdd03648882a3a2201263d65b230916567ee4096
Sachin Gupta <sachin070gupta@gmail.com>
2021-03-10 17:45:13 +0530

    Add files via upload

---
+++ src/com/cg/util/BankingExceptions.java
@@ -0,0 +1,7 @@
+package com.cg.util;
+
+public class BankingExceptions extends Exception{
+	public BankingExceptions(String message) {
+		super(message);
+	}
+}
---
+++ src/com/cg/util/BankingMessages.java
@@ -0,0 +1,9 @@
+package com.cg.util;
+
+public class BankingMessages {
+	public static final String INSUFFICIENT_BALANCE = 
+			"Insufficient Balance in Account";
+	
+	public static final String ACCOUNT_NOT_FOUND = 
+			"Account not found ";
+}
